9a3c5a5e5abfb61d4d42ad20c6d0b029eaa50087
HiddenRoom <bestwitherskele@gmail.com>
2023-03-10 03:32:35 -0700

    typo

--- src/sub.v
+++ src/sub.v
@@ -52,14 +52,14 @@ module subEight
 
   wire [6:0] borrows;
 
-  halfSubtractor subtractor0(.a(d0[0]), .b(d1[0]), .diff(resTmp[0]), .bOut(borrows[0]));
-  fullSubtractor subtractor1(.a(d0[1]), .b(d1[1]), .bIn(borrows[0]), .diff(resTmp[1]), .bOut(borrows[1]));
-  fullSubtractor subtractor2(.a(d0[2]), .b(d1[2]), .bIn(borrows[1]), .diff(resTmp[2]), .bOut(borrows[2]));
-  fullSubtractor subtractor3(.a(d0[3]), .b(d1[3]), .bIn(borrows[2]), .diff(resTmp[3]), .bOut(borrows[3]));
-  fullSubtractor subtractor4(.a(d0[4]), .b(d1[4]), .bIn(borrows[3]), .diff(resTmp[4]), .bOut(borrows[4]));
-  fullSubtractor subtractor5(.a(d0[5]), .b(d1[5]), .bIn(borrows[4]), .diff(resTmp[5]), .bOut(borrows[5]));
-  fullSubtractor subtractor6(.a(d0[6]), .b(d1[6]), .bIn(borrows[5]), .diff(resTmp[6]), .bOut(borrows[6]));
-  fullSubtractor subtractor7(.a(d0[7]), .b(d1[7]), .bIn(borrows[6]), .diff(resTmp[7]), .bOut(bOut));
+  halfSubtractor subtractor0(.a(dIn0[0]), .b(dIn1[0]), .diff(resTmp[0]), .bOut(borrows[0]));
+  fullSubtractor subtractor1(.a(dIn0[1]), .b(dIn1[1]), .bIn(borrows[0]), .diff(resTmp[1]), .bOut(borrows[1]));
+  fullSubtractor subtractor2(.a(dIn0[2]), .b(dIn1[2]), .bIn(borrows[1]), .diff(resTmp[2]), .bOut(borrows[2]));
+  fullSubtractor subtractor3(.a(dIn0[3]), .b(dIn1[3]), .bIn(borrows[2]), .diff(resTmp[3]), .bOut(borrows[3]));
+  fullSubtractor subtractor4(.a(dIn0[4]), .b(dIn1[4]), .bIn(borrows[3]), .diff(resTmp[4]), .bOut(borrows[4]));
+  fullSubtractor subtractor5(.a(dIn0[5]), .b(dIn1[5]), .bIn(borrows[4]), .diff(resTmp[5]), .bOut(borrows[5]));
+  fullSubtractor subtractor6(.a(dIn0[6]), .b(dIn1[6]), .bIn(borrows[5]), .diff(resTmp[6]), .bOut(borrows[6]));
+  fullSubtractor subtractor7(.a(dIn0[7]), .b(dIn1[7]), .bIn(borrows[6]), .diff(resTmp[7]), .bOut(bOut));
 
   assign dOut = resTmp & {8{enable}};
   

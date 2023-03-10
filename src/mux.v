cc5b98421b0aa1d73220a9ddc01b9be90f5e052e
HiddenRoom <bestwitherskele@gmail.com>
2023-03-10 03:17:32 -0700

    fix include guard on mux.v

--- src/mux.v
+++ src/mux.v
@@ -58,17 +58,4 @@ module fourOneMux
 
 endmodule
 
-module xorEight
-(
-  input [7:0] dIn0,
-  input [7:0] dIn1,
-  input enable,
-
-  output [7:0] dOut
-);
-
-  assign dOut = (dIn0 ^ dIn1) & {8{enable}};
-
-endmodule
-
 `endif

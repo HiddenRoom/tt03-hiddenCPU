f91ac75c019beade32027dc0d6f6611738ef740e
HiddenRoom <bestwitherskele@gmail.com>
2023-03-10 02:41:40 -0700

    fixed implicit declare

--- src/decode.v
+++ src/decode.v
@@ -5,12 +5,12 @@ module twoFourDecode
   output [3:0] enable
 );
 
-  wire [1:0] selI;
+  wire [1:0] notSel;
 
   assign notSel[0] = ~sel[0];
-  assign notSel[1] = ~i[1];
+  assign notSel[1] = ~sel[1];
 
-  assign enable[0] = notSel[1] & notSe[0];
+  assign enable[0] = notSel[1] & notSel[0];
   assign enable[1] = notSel[1] & sel[0];
   assign enable[2] = sel[1] & notSel[0];
   assign enable[3] = sel[1] & sel[0];

e21e6ea1090cbd6062da73f360a63a93026fa496
HiddenRoom <bestwitherskele@gmail.com>
2023-03-15 21:52:58 -0600

    test actually works and does smth :)))))))

--- src/test.py
+++ src/test.py
@@ -9,6 +9,9 @@ async def test_my_design(dut):
     cocotb.start_soon(clock.start());
     dut._log.info("making sure reset works as intended");
     dut.rst.value = 1;
-    dut.instruction.value = 52;
-    await ClockCycles(dut.clk, 3);
+    dut.instruction.value = 0b110110; 
+    await ClockCycles(dut.clk, 1);
+    dut.rst.value = 0;
+    await ClockCycles(dut.clk, 5);
     dut._log.info(dut.outBus.value);
+    assert dut.outBus.value == 0b00000100; # outputting pc 5 cycles after reset so it should be 5

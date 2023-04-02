ad790e1880ac9eaebec76a60b1e29ad7a77dd1d0
HiddenRoom <bestwitherskele@gmail.com>
2023-04-02 04:29:21 -0600

    labeled instructions being tested

--- src/test.py
+++ src/test.py
@@ -16,14 +16,15 @@ async def test_my_design(dut):
     dut.instruction.value = 0b001101;
     await ClockCycles(dut.clk, 1);
     dut._log.info(dut.outBus.value);
-    await RisingEdge(dut.clk);
-    assert dut.outBus.value == 0b00000100; # r3 will be 4 since it got the 1 in r1 added to it
     dut.instruction.value = 0b001001; # increasing memory pointer to uninitialized location
     await ClockCycles(dut.clk, 1);
     dut._log.info(dut.outBus.value);
+    dut._log.info("test of add: opcode 0b00");
+    assert dut.outBus.value == 0b00000100; # r3 will be 4 since it got the 1 in r1 added to it
     dut.instruction.value = 0b111010;
     await ClockCycles(dut.clk, 1);
     dut._log.info(dut.outBus.value);
+    dut._log.info("test of subtraction via return to initialized memory location: opcode 0b01");
     dut.instruction.value = 0b011001; # decrementing to the initialized location
     await ClockCycles(dut.clk, 1);
     dut._log.info("should be xxxxxxxx for 2 below");
@@ -33,6 +34,7 @@ async def test_my_design(dut):
     dut._log.info(dut.outBus.value);
     dut.instruction.value = 0b001011; # r2 + r3 = 5
     await ClockCycles(dut.clk, 1);
+    dut._log.info("test of memory read/write: opcode (extended for special operation) (read) 0b111010, (write) 0b110101");
     assert dut.outBus.value == 0b00000011; # r3 will be written to the state it had before being added to
     dut._log.info(dut.outBus.value);
     dut.instruction.value = 0b001011; # r2 + r3 = 8
@@ -44,6 +46,7 @@ async def test_my_design(dut):
     dut.instruction.value = 0b001111;
     await ClockCycles(dut.clk, 1);
     dut._log.info(dut.outBus.value);
+    dut._log.info("test of xor: opcode 0b10");
     assert dut.outBus.value == 0b00001011;
     dut.instruction.value = 0b001111;
     await ClockCycles(dut.clk, 1);
@@ -54,6 +57,7 @@ async def test_my_design(dut):
     dut.instruction.value = 0b111111;
     await ClockCycles(dut.clk, 1);
     dut._log.info(dut.outBus.value);
+    dut._log.info("test of swapping output to program counter: opcode (extended for special operation) 0b111111");
     assert dut.outBus.value == 0b00001101;
     dut.instruction.value = 0b011010; # r2 - r2 = 0
     await ClockCycles(dut.clk, 1);
@@ -65,6 +69,7 @@ async def test_my_design(dut):
     dut.instruction.value = 0b111101; # mov r3, r1  r3 = 1
     await ClockCycles(dut.clk, 1);
     dut._log.info(dut.outBus.value);
+    dut._log.info("test of mov: opcode 0b11");
     assert dut.outBus.value == 0b00000000; # should be zero after moving r2 into r3 after zeroing r2
     dut.rst.value = 1;
     await ClockCycles(dut.clk, 1);
@@ -101,6 +106,7 @@ async def test_my_design(dut):
     dut._log.info(dut.outBus.value);
     await RisingEdge(dut.clk);
     dut._log.info(dut.outBus.value);
+    dut._log.info("test of branch if carry: opcode (extended for special operation) 0b110000");
     assert dut.outBus.value == 0b00001100;
 
     '''
